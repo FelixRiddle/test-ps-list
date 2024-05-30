34387fb25add4c40885ce624d0eef983b8634c33
Felix <fedecraft3312@gmail.com>
2024-05-30 12:46:28 -0300

    Get node processes cwd

--- index.js
+++ index.js
@@ -1,13 +1,146 @@
 import psList from "ps-list";
+import fs from "fs";
+import os from "os";
+
+/**
+ * Get process working directory
+ * 
+ * @param {*} pid 
+ * @param {*} callback 
+ */
+export function processDirectory(pid, callback) {
+    switch (os.type()) {
+        case 'Linux':
+            fs.readlink('/proc/' + pid + '/cwd', callback);
+            break;
+        case 'Darwin':
+            exec('lsof -a -d cwd -p ' + pid + ' | tail -1 | awk \'{print $9}\'', callback);
+            break;
+        default:
+            callback('unsupported OS');
+            break;
+    }
+}
 
 /**
  * Get node processes
+ * 
+ * I can't get the process cwd 😡😡😡😡
  */
-async function nodeProcesses() {
+export async function nodeProcesses(callback) {
     const pl = await psList();
-    const nodeProcs = pl.filter((process) => process.name === "node");
-    console.log(`Node processes: `, nodeProcs);
+    const nodeProcessesName = ["node", "nodemon", "ts-node"]
+    
+    const nodeProcs = pl.filter((process) => nodeProcessesName.includes(process.name));
+    
+    const processesWithCwd = nodeProcs.map((proc) => {
+        processDirectory(proc.pid, (err, res) => {
+            if(err) {
+                console.log(err);
+            } else {
+                proc.cwd = res;
+            }
+        });
+        
+        return proc;
+    });
+    
+    callback(processesWithCwd);
 }
 
-console.log(`Showing node processses`);
-nodeProcesses();
+/**
+ * Get node processes with cwd
+ * 
+ * The problem with this one is that it gets processes randomly one by one.
+ */
+export async function randomAccessNodeProcesses(callback) {
+    const pl = await psList();
+    const nodeProcessesName = ["node", "nodemon", "ts-node"]
+    
+    const nodeProcs = pl.filter((process) => nodeProcessesName.includes(process.name));
+    
+    nodeProcs.map((proc) => {
+        processDirectory(proc.pid, (err, res) => {
+            if(err) {
+                console.log(err);
+                
+                return callback(proc);
+            } else {
+                proc.cwd = res;
+                
+                return callback(proc);
+            }
+        });
+    });
+}
+
+/**
+ * Abstraction of forced await
+ * 
+ * @param {Array} processes Array of processes retrieved with ps-list packages(or if they have 'pid' property, then it'll work too) 
+ * @param {function} callback The callback to send all processes when retrieved
+ */
+export function forcedAwaitCwdRetrieval(processes, callback) {
+    // We will wait until we retrieve all processes and then call the user given callback
+    let updatedProcesses = [];
+    const awaitCallback = (proc) => {
+        updatedProcesses.push(proc);
+        
+        // Check if we've got em' all
+        // Gotta catch em' all!
+        if(updatedProcesses.length === processes.length) {
+            // Alright send em' back!
+            return callback(updatedProcesses);
+        }
+    }
+    
+    processes.map((proc) => {
+        processDirectory(proc.pid, (err, res) => {
+            if(err) {
+                console.log(err);
+                
+                return awaitCallback(proc);
+            } else {
+                proc.cwd = res;
+                
+                return awaitCallback(proc);
+            }
+        });
+    });
+}
+
+/**
+ * Get node processes with cwd forced await
+ * 
+ */
+export async function nodeProcessesForcedAwait(callback) {
+    const pl = await psList();
+    const nodeProcessesName = ["node", "nodemon", "ts-node"]
+    
+    const nodeProcs = pl.filter((process) => nodeProcessesName.includes(process.name));
+    
+    // Force the await of all processes retrieval
+    forcedAwaitCwdRetrieval(nodeProcs, callback);
+}
+
+/**
+ * Shell processses
+ */
+async function shellProcesses() {
+    const pl = await psList();
+    const nodeProcs = pl.filter((process) => process.name === "sh");
+    console.log(`Shell processes: `, nodeProcs);
+}
+
+(async () => {
+    console.log(`Shell processes`);
+    
+    // Shell processses
+    await shellProcesses();
+    
+    console.log(`\nNode processses`);
+    
+    await nodeProcessesForcedAwait((procs) => {
+        console.log(`Node processes: `, procs);
+    })
+})();
